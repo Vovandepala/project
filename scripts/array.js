ebb5b2e9c951122526dcafa70d887739de6f9e53
Vova <golovakhaua@gmail.com>
2023-06-02 15:06:29 +0300

    add array & if_else

---
+++ scripts/array.js
@@ -0,0 +1,44 @@
+// object {}
+
+// const obj = {
+//       key1: 123.,
+//       key2: 'qweas',
+//       key3: true
+// }
+
+// console.log(obj)
+
+
+const cart = {
+      money:{
+            sum: 500,
+            currency: 'Rub',
+            allowCurrency: ['Rub', 'Evro'],
+            discount: {
+                  hasDiscount: true,
+                  value: 30
+            },
+      },
+      items: [{
+            title: 'pen',
+            count: 5,
+            price: 1
+      },
+       {
+            title: 'book',
+            count: 2,
+            price: 10
+      }],
+};
+
+cart['name'] = 'Vova'
+
+console.log(cart)
+
+// console.log(cart.money.discount.value)
+// console.log(cart.items[0].count)
+
+
+// console.log(cart['money']['sum']);
+// console.log(cart.money.sum)
+
